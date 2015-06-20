d2f3507557d60762b4033e3d8c20e89fa39ed988
Evan Tschuy <evantschuy@gmail.com>
2015-06-20 00:49:14 -0700

    get object or 404

--- gpi/gpi_web/views.py
+++ gpi/gpi_web/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 import json
 
 from gpi.gpi_web.models import Release, Project
 from gpi.gpi_web.serializer import PackageSerializer
 
 def project_details(request, name=None):
-    project = Project.objects.get(package_name=name)
+    project = get_object_or_404(Project, package_name=name)
 
     serializer = PackageSerializer()
 
