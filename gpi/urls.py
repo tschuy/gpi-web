4623329fdce0cf99f10275329cbbdae259ebbbd1
Evan Tschuy <evantschuy@gmail.com>
2015-06-23 20:28:38 -0700

    Add static and media to urls only if django is in debug

--- gpi/urls.py
+++ gpi/urls.py
@@ -27,5 +27,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
